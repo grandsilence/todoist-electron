957ef854f431d3ed640df894db62060984244ddd
grandsilence <mixtape774@gmail.com>
2018-07-23 10:12:52 +0300

    Inject CSS via file

--- main.js
+++ main.js
@@ -1,36 +1,25 @@
 const {app, BrowserWindow} = require('electron');
+const fs = require('fs');
+const path = require('path');
+
 var mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, frame: false});
-
-  // и загрузит index.html приложение.
-  mainWindow.loadURL('https://todoist.com');
-  mainWindow.webContents.on('did-finish-load', function() {
-    mainWindow.webContents.insertCSS(`
-
-    `);
+  mainWindow = new BrowserWindow({
+    width: 800, height: 600, frame: false, center: true, show: false
   });
+
+  mainWindow.loadURL('https://todoist.com/');
+  //mainWindow.webContents.on('did-finish-load', function() {});
 
   mainWindow.webContents.on('dom-ready', function() {
-    mainWindow.webContents.insertCSS(`
-    body { font-family: Arial, Tahoma, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol" !important; }
+    // Inject CSS
+    const css = fs.readFileSync(path.join(__dirname, 'assets/styles/override.css'), 'utf8');
+    mainWindow.webContents.insertCSS(css);
 
-    /* Draggable elements */
-    #top_bar, #left_menu, #content, #editor
-    { -webkit-app-region: drag; }
-
-    /* Disable drag for some inner elements */
-    #top_icons, #agenda_view, #quick_find, .left_menu_toggle, #list_holder
-    { -webkit-app-region: no-drag; }
-    `);
+    mainWindow.show();
   });
-
- 
-
-  //let contents = win.webContents;
-  //contents.getElementById('top_bar').style = "display:none";
 }
 
 app.on('ready', createWindow);
