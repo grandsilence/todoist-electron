76f0bed2ac3d04595c1d6d1a0f3014c2d69f04dd
grandsilence <mixtape774@gmail.com>
2018-07-23 11:42:18 +0300

    Block Google Analytics

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, session} = require('electron');
 const fs = require('fs');
 const path = require('path');
 
 var mainWindow;
-
 function createWindow () {
-  // Create the browser window.
+  // Create the browser window
   mainWindow = new BrowserWindow({
     width: 800, height: 600, frame: false, center: true, show: false
   });
-
+  
+  // ready-to-show /*mainWindow.once('ready-to-show', function() {});*/
   mainWindow.loadURL('https://todoist.com/');
-  //mainWindow.webContents.on('did-finish-load', function() {});
 
   mainWindow.webContents.on('dom-ready', function() {
     // Inject CSS
@@ -19,7 +18,18 @@ function createWindow () {
     mainWindow.webContents.insertCSS(css);
 
     mainWindow.show();
+    mainWindow.focus();
   });
 }
 
-app.on('ready', createWindow);
+app.on('ready', function() {
+  // Disable Google Analytics 
+  const filter = {
+    urls: ['*://*.google-analytics.com/*']
+  };
+  session.defaultSession.webRequest.onBeforeRequest(filter, (details, callback) => {
+    callback({cancel: true})
+  });
+  // Initilize window and open todoist
+  createWindow();
+});
